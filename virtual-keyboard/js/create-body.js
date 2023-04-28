e1fd5d87513694ad07a003939fbf1ec232af94b8
whowouldwin <leranetwork@gmail.com>
2023-04-28 23:43:32 +0300

    fix: implement createBody function (add textContent)

--- virtual-keyboard/js/create-body.js
+++ virtual-keyboard/js/create-body.js
@@ -1,15 +1,18 @@
 const createBody = () => {
-  function createElementWithClass(type, className) {
+  function createElementWithClass(type, className, text) {
     const element = document.createElement(type);
-    element.className = className;
+    element.classList.add(...className);
+    if (text) {
+      element.textContent = text;
+    }
     return element;
   }
-  const wrapper = createElementWithClass('div', 'wrapper');
-  const title = createElementWithClass('h1', 'Virtual Keyboard');
-  const textarea = createElementWithClass('textarea', 'textarea');
-  const keyboard = createElementWithClass('div', 'keyboard');
-  const description = createElementWithClass('p', 'description');
-  const language = createElementWithClass('p', 'language');
+  const wrapper = createElementWithClass('div', ['wrapper']);
+  const title = createElementWithClass('h1', ['title'], 'RSS Virtual Keyboard');
+  const textarea = createElementWithClass('textarea', ['textarea']);
+  const keyboard = createElementWithClass('div', ['keyboard']);
+  const description = createElementWithClass('p', ['description'], 'The keyboard was created in the Windows operating system.');
+  const language = createElementWithClass('p', ['language'], 'To switch language, use the combination: left ctrl + alt.');
   wrapper.append(title, textarea, keyboard, description, language);
   document.body.append(wrapper);
   console.log(wrapper);
